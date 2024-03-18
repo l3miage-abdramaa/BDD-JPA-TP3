08365fe0596a7e9a3c8d6a37fa9ab61a9580e318
abdramaa <abakar.abdraman@etu.univ-grenoble-alpes.fr>
2024-03-18 17:00:37 +0100

    correction exo1 reste encore

---
+++ server/src/main/java/fr/uga/l3miage/tp3/exo1/components/AlbumComponent.java
@@ -0,0 +1,63 @@
+package fr.uga.l3miage.tp3.exo1.components;
+
+import fr.uga.l3miage.tp3.exo1.models.AlbumEntity;
+import fr.uga.l3miage.tp3.exo1.repositories.AlbumRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.sql.Date;
+import java.util.Set;
+
+@Component
+@RequiredArgsConstructor
+public class AlbumComponent {
+
+    private final AlbumRepository albumRepository;
+
+    // opérations CRUD
+
+    // READ
+    private AlbumEntity getAlbum(String title) {
+        return albumRepository
+                .findById(title)
+                .orElseThrow();
+    }
+
+    // CREATE
+
+    public AlbumEntity createAlbum() {
+        AlbumEntity albumEntity = AlbumEntity
+                .builder()
+                .title("fire")
+                .releaseDate(Date.valueOf("03/05/2000"))
+                .songEntities(Set.of())
+                .artistEntity(null)
+                .build();
+        return albumRepository.save(albumEntity);
+    }
+
+    // UPDATE
+
+    public AlbumEntity updateAlbumDateRelease(String title,Date newReleaseDate) {
+        AlbumEntity albumEntity = albumRepository
+                .findById(title)
+                .orElseThrow();
+        albumEntity.setReleaseDate(newReleaseDate);
+        return albumRepository.save(albumEntity);
+    }
+
+    // DELETE ALBUM
+
+    public void deleteAlbum(String title) {
+        albumRepository.deleteById(title);
+    }
+
+
+
+    //
+
+    public Set<AlbumEntity> getAlbumByTitle(String title) {
+        return albumRepository.findAllByTitle(title);
+    }
+
+}
---
+++ server/src/main/java/fr/uga/l3miage/tp3/exo1/components/ArtistComponent.java
@@ -0,0 +1,43 @@
+package fr.uga.l3miage.tp3.exo1.components;
+
+import fr.uga.l3miage.tp3.exo1.enums.GenreMusical;
+import fr.uga.l3miage.tp3.exo1.models.ArtistEntity;
+import fr.uga.l3miage.tp3.exo1.repositories.ArtistRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+@RequiredArgsConstructor
+public class ArtistComponent {
+
+    private final ArtistRepository artistRepository;
+
+    // READ
+
+    private ArtistEntity getArtist(String name){
+        return artistRepository
+                .findById(name)
+                .orElseThrow();
+    }
+
+    // CREATE
+
+    public ArtistEntity createArtist(ArtistEntity artistEntity) {
+        return artistRepository.save(artistEntity);
+    }
+
+    // UPDATE
+
+    private ArtistEntity updateArtisteBiography(String name,String newBiography) {
+        ArtistEntity artistEntity = artistRepository
+                .findById(name)
+                .orElseThrow();
+        artistEntity.setBiography(newBiography);
+        return artistRepository.save(artistEntity);
+    }
+
+    // DELETE
+
+}
